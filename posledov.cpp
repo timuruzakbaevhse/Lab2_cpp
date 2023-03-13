48b0d91084b26250f2405893f157cd49008e8d48
timuruzakbaevhse <91337323+timuruzakbaevhse@users.noreply.github.com>
2023-03-13 18:20:17 +0300

    Add files via upload

---
+++ posledov.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+#include <string>
+#include <stack>
+
+int main() {
+    std::string brackets;
+    std::cin >> brackets;
+
+    std::stack<char> stack;
+
+    for (char bracket : brackets) {
+        switch (bracket) {
+        case '(':
+        case '[':
+        case '{':
+            stack.push(bracket);
+            break;
+        case ')':
+            if (!stack.empty() && stack.top() == '(') {
+                stack.pop();
+            }
+            else {
+                std::cout << "no" << std::endl;
+                return 0;
+            }
+            break;
+        case ']':
+            if (!stack.empty() && stack.top() == '[') {
+                stack.pop();
+            }
+            else {
+                std::cout << "no" << std::endl;
+                return 0;
+            }
+            break;
+        case '}':
+            if (!stack.empty() && stack.top() == '{') {
+                stack.pop();
+            }
+            else {
+                std::cout << "no" << std::endl;
+                return 0;
+            }
+            break;
+        }
+    }
+
+    if (stack.empty()) {
+        std::cout << "yes" << std::endl;
+    }
+    else {
+        std::cout << "no" << std::endl;
+    }
+
+    return 0;
+}
