59a1caeef1fcd3be0a945924af6fc0fee2b54cd5
timuruzakbaevhse <91337323+timuruzakbaevhse@users.noreply.github.com>
2023-03-13 17:57:11 +0300

    Add files via upload

---
+++ Task1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    set<int> numbers;
+    // создаем set (множество), в котором будем хранить уникальные числа
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        numbers.insert(x);
+    }
+    cout << numbers.size() << endl;
+    // выводим количество уникальных чисел - размер set'а
+    return 0;
+}
